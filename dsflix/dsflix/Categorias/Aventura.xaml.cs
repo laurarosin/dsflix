8de8d1c09d5ae2e087102901c608aca967efb833
Usuario <Usuario@CPD03-M03>
2022-05-12 15:55:25 -0300

    Adicionar arquivos de projeto.

---
+++ dsflix/dsflix/Categorias/Aventura.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace dsflix.Categorias
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Aventura : ContentPage
+    {
+        private ImageSource btnAladdin_Source;
+
+        public Aventura()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            logo.Source = ImageSource.FromResource("EtecFlixApp.Img.etecflix.png");
+
+            btnAladdin_Source = ImageSource.FromResource("EtecFlixApp.Posters.aladdin.jpg");
+            btnCapitaMarvel.Source = ImageSource.FromResource("EtecFlixApp.Posters.CapitaMarvel.jpg");
+            btnPanteraNegra.Source = ImageSource.FromResource("EtecFlixApp.Posters.PanteraNegra.jpg");
+            btnShazam.Source = ImageSource.FromResource("EtecFlixApp.Posters.Shazam.jpg");
+        }
+
+        private async void btnCapitaMarvel_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new Filmes.CapitaMarvel());
+            }catch (Exception ex)
+            {
+                await DisplayAlert("Ops, ocorreu um erro...", ex.Message.ToString(), "Ok");
+            }
+        }
+        private async void btnPanteraNegra_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new Filmes.PanteraNegra());
+            }catch(Exception ex)
+            {
+                await DisplayAlert("Ops. ocorreu um erro...", ex.Message.ToString(), "Ok");
+            }
+
+        }
+        private async void btnShazam_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new Filmes.Shazam());
+            }catch(Exception ex)
+            {
+                await DisplayAlert("Ops, ocorreu um erro...", ex.Message.ToString(), "Ok");
+            }
+            
+        }
+        private async void btnAladdin_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new Filmes.Aladdin());
+            }catch(Exception ex)
+            {
+                await DisplayAlert("Ops, ocorreu um erro...", ex.Message.ToString(), "Ok");
+            }
+
+        }
+    }
+}
---
+++ dsflix/dsflix/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using dsflix.Categorias;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace dsflix
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            logo.Source = ImageSource.FromResource("dsflix.Img.netflix.jpg");
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+        private async void Btn_Open_Aventura(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new Aventura());
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops, ocorre um erro...", ex.Message.ToString(), "OK");
+            }
+        }
+        private async void Btn_Open_Comedia(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new Comédia());
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops, ocorreu um erro...", ex.Message.ToString(), "OK");
+            }
+
+        }  
+        private async void Btn_Open_Drama(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new Drama());    
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Ops, ocorreu um erro...",ex.Message.ToString(), "OK");  
+            }
+        }
+
+        private async void Btn_Open_Romance(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new Romance());
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops, ocorreu um erro...", ex.Message, "OK");
+            }
+
+
+
+        }
+
+        private async void Btn_Open_Terror(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new Terror());
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops, ocorreu um erro...", ex.Message, "OK");
+            }
+
+        }
+    }
+}
